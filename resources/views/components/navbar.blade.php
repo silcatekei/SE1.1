d43623da216686884f7d03dcf77d303586ceadd8
silcatekei <mattykie27@gmail.com>
2025-02-25 01:16:18 +0800

    minor changes with errors

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,41 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('assets/SPI_LOGO-removebg-preview.png') }}" alt="School Logo" style="max-height: 40px; margin-right: 5px;">  <!-- Added style for logo size -->
+            SKILL-POWER INSTITUTE SHS
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#features">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#admission">Admission</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" id="studentPortalLink" data-bs-toggle="modal" data-bs-target="#studentPortalModal">Student Portal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" id="" data-bs-toggle="modal" data-bs-target="">Admin Portal</a>
+                </li>
+                <li class="nav-item">
+                    <label class="switch" style="margin-left: 10px;">  <!-- Added margin -->
+                        <input type="checkbox" id="darkModeToggle">
+                        <span class="slider round"></span>
+                    </label>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
